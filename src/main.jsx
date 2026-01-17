cc8274cc939c4a475246ac9eb0db9d98251f7c3d
Thalissom Vinicius <102325856+thalissomvinicius@users.noreply.github.com>
2026-01-17 10:56:17 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights />
     <BrowserRouter>
       <AuthProvider>
         <App />
